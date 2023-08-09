d895b416635d70d537dd896aa95d2b0c2b4a8bee
ki8vi <oneofbln@gmail.com>
2023-08-09 09:09:18 +0600

    feat: add solution for brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let splited = str.split("");
+  let bracketsExs = [];
+  for(let i = 0; i < bracketsConfig.length; i++){
+    bracketsExs.push(bracketsConfig[i].join(""))
+  }
+  
+  for(let i = 0; i < splited.length; i++) {
+   
+    if(bracketsExs.includes(stack[stack.length-1] + splited[i])) {
+      stack.pop()
+    } else {
+       stack.push(splited[i]);
+    }
+  }
+  return !stack.length
 }
